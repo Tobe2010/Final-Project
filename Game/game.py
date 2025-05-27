1edb992636de85836945fa78627602aadd6ad4c1
tobemuoto <tobemuoto@gmail.com>
2025-05-27 10:01:14 -0400

    Update game.py

--- Game/game.py
+++ Game/game.py
@@ -1,3 +1,9 @@
 import pygame
 import pymunk
 import sqlite3
+
+class Ball():
+    def __init__(self, x, color):
+        self.color = color
+        self.body = pymunk.Body()
+        self.body.postition = x, 400
