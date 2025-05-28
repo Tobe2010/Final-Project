ca110e83271405bf77a880714b8c9e71e46468a7
tobemuoto <tobemuoto@gmail.com>
2025-05-28 09:49:57 -0400

    Update game.py

--- Game/game.py
+++ Game/game.py
@@ -8,11 +8,11 @@ display = pygame.display.set_mode((800, 800))
 
 #Initialize pymunk space
 space = pymunk.Space()
-space.gravity = 0, -10
+space.gravity = 0, 100
 
 # Convert pymunk position to pygame position
 def convert_pos(coordinates):
-    return int(coordinates[0]), 800 - int(coordinates[1])
+    return int(coordinates[0]), int(coordinates[1])
 
 #Frame count handling
 clock = pygame.time.Clock()
@@ -20,52 +20,52 @@ FPS = 80
 
 
 class Ball():
-    def __init__(self, x, color):
+    def __init__(self, color, space):
         self.color = color
         self.body = pymunk.Body(1, 1666)
-        self.body.postition = x, 400
+        self.body.postition = 400, 400
         self.shape = pymunk.Circle(self.body, 10)
         self.shape.elasticity = 1
        
         space.add(self.body, self.shape)
         
-    def printPos(self):
-        print(self.body.position)
+
+    def draw(self, display, x, shouldJump):        
+        pygame.draw.circle(display, self.color, (x, self.body.position[1]), 15)
         
-    def pos(self):
-        return self.body.position
-    
-    def draw(self):        
-        pygame.draw.circle(display, self.color, self.body.postition, 15)
+        if shouldJump:
+            self.body.apply_impulse_at_local_point(pymunk.Vec2d(0, -1))
         
 
 #class Rectangle():
     #def __init__(self, )
     
     
-ball = Ball(400, (255, 0, 0))
+ball = Ball((255, 0, 0), space)
         
 def game():
     sc = 0
+    jump = False
     
     while True:
         space.step(0.02)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return sc
+                return 
             
-        display.fill((255, 255, 255))
-                    
-
-        pygame.draw.circle(display, (255,0, 0), convert_pos(ball.pos()), 15)
+            if event.type == pygame.KEYDOWN:
+                jump = event.key == pygame.K_SPACE
         
+        display.fill((255, 255, 255))
+        
+        ball.draw(display, 400, jump)
+                
         pygame.display.update()
         clock.tick(FPS)
         
         sc += 1
         
-        ball.printPos()
         
 
 # Enter x into the table under the "score" column
